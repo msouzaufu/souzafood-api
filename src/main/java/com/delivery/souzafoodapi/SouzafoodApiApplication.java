7c361d4a77dc5029a01afc41a4d7075e56629104
Matheus Souza <matheus-rodrigues-00@hotmail.com>
2020-09-20 11:24:03 -0300

    Initial commit

---
+++ src/main/java/com/delivery/souzafoodapi/SouzafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.souzafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouzafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouzafoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery/souzafoodapi/SouzafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.souzafoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouzafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
