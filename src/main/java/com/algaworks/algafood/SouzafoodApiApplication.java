298517d082985bc3d39b5b81bea88dc11f368364
Matheus Souza <matheus-rodrigues-00@hotmail.com>
2021-04-07 21:31:33 -0300

    Ajustes na aplicação

--- src/main/java/com/algaworks/algafood/SouzafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/SouzafoodApiApplication.java
@@ -1,4 +1,4 @@
-package com.delivery.souzafoodapi;
+package com.algaworks.algafood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
